1c969f981defcab92510004be15dfabb3b32502a
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2019-12-26 15:39:25 +1030

    Move remaining functions to lib

--- Resources/completedReportLib.js
+++ Resources/completedReportLib.js
@@ -68,6 +68,67 @@ var _ = (function() {
     URL.fromString(dayOneUrlStr).call(() => {});
   };
 
+  completedReportLib.runReport = resultUrl => {
+    functionLibrary = PlugIn.find("com.KaitlinSalzke.functionLibrary").library(
+      "functionLibrary"
+    );
+
+    var now = new Date();
+    var today = Calendar.current.startOfDay(now);
+    var yesterday = functionLibrary.removeOneDayFromDate(today);
+
+    // basic selection form - select today, tomorrow, or other
+    var selectDayForm = new Form();
+    selectDayPopupMenu = new Form.Field.Option(
+      "selectedDay",
+      "Day",
+      ["Today", "Yesterday", "Other"],
+      null,
+      "Today"
+    );
+    selectDayForm.addField(selectDayPopupMenu);
+    selectDayFormPrompt = "Which day?";
+    selectDayFormPromise = selectDayForm.show(selectDayFormPrompt, "Continue");
+
+    // form for when 'other' is selected - to enter alternative date
+    var selectOtherDateForm = new Form();
+    selectOtherDateDateField = new Form.Field.Date("dateInput", "Date", today);
+    selectOtherDateForm.addField(selectOtherDateDateField);
+    selectOtherDateFormPrompt = "Select date:";
+
+    // show forms
+    selectDayFormPromise.then(function(formObject) {
+      optionSelected = formObject.values["selectedDay"];
+      console.log(optionSelected);
+      console.log(today);
+      switch (optionSelected) {
+        case "Today":
+          runReportForDay(today);
+          break;
+        case "Yesterday":
+          runReportForDay(yesterday);
+          break;
+        case "Other":
+          selectOtherDateFormPromise = selectOtherDateForm.show(
+            selectOtherDateFormPrompt,
+            "Continue"
+          );
+          selectOtherDateFormPromise.then(function(formObject) {
+            runReportForDay(formObject.values["dateInput"]);
+          });
+          selectOtherDateFormPromise.catch(function(err) {
+            console.log("form cancelled", err.message);
+          });
+          break;
+        default:
+      }
+    });
+
+    selectDayFormPromise.catch(function(err) {
+      console.log("form cancelled", err.message);
+    });
+  };
+
   return completedReportLib;
 })();
 _;
--- Resources/sendToDayOne.js
+++ Resources/sendToDayOne.js
@@ -7,67 +7,9 @@ var _ = (function() {
 
     // FUNCTIONS FROM LIBRARY
     lib = this.completedReportLib;
-    getTasksCompletedOnDate = lib.getTasksCompletedOnDate;
-    runReportForDay = lib.runReportForDay;
+    runReport = lib.runReport;
 
-    functionLibrary = PlugIn.find("com.KaitlinSalzke.functionLibrary").library(
-      "functionLibrary"
-    );
-
-    var now = new Date();
-    var today = Calendar.current.startOfDay(now);
-    var yesterday = functionLibrary.removeOneDayFromDate(today);
-
-    // basic selection form - select today, tomorrow, or other
-    var selectDayForm = new Form();
-    selectDayPopupMenu = new Form.Field.Option(
-      "selectedDay",
-      "Day",
-      ["Today", "Yesterday", "Other"],
-      null,
-      "Today"
-    );
-    selectDayForm.addField(selectDayPopupMenu);
-    selectDayFormPrompt = "Which day?";
-    selectDayFormPromise = selectDayForm.show(selectDayFormPrompt, "Continue");
-
-    // form for when 'other' is selected - to enter alternative date
-    var selectOtherDateForm = new Form();
-    selectOtherDateDateField = new Form.Field.Date("dateInput", "Date", today);
-    selectOtherDateForm.addField(selectOtherDateDateField);
-    selectOtherDateFormPrompt = "Select date:";
-
-    // show forms
-    selectDayFormPromise.then(function(formObject) {
-      optionSelected = formObject.values["selectedDay"];
-      console.log(optionSelected);
-      console.log(today);
-      switch (optionSelected) {
-        case "Today":
-          runReportForDay(today);
-          break;
-        case "Yesterday":
-          runReportForDay(yesterday);
-          break;
-        case "Other":
-          selectOtherDateFormPromise = selectOtherDateForm.show(
-            selectOtherDateFormPrompt,
-            "Continue"
-          );
-          selectOtherDateFormPromise.then(function(formObject) {
-            runReportForDay(formObject.values["dateInput"]);
-          });
-          selectOtherDateFormPromise.catch(function(err) {
-            console.log("form cancelled", err.message);
-          });
-          break;
-        default:
-      }
-    });
-
-    selectDayFormPromise.catch(function(err) {
-      console.log("form cancelled", err.message);
-    });
+    report = runReport("aaa");
   });
 
   action.validate = function(selection, sender) {
