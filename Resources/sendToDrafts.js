d319e3c08742edd63ae211483500d29d7cba0c1d
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2020-04-26 14:15:10 +1000

    Update validation so does not show in share menu

--- Resources/sendToDrafts.js
+++ Resources/sendToDrafts.js
@@ -13,7 +13,8 @@
   });
 
   action.validate = function (selection, sender) {
-    return true;
+    // only valid if nothing is selected - so does not show in share menu
+    return selection.tasks.length == 0 && selection.projects.length == 0;
   };
 
   return action;
